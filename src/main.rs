05d56b7194bbc528bf76d4f14ad3bcdb65986a40
irlg0at <34748847+irlg0at@users.noreply.github.com>
2024-11-16 21:10:41 +0100

    Work on reqwest shodan call

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-use std::fs;
+use std::fs::{self, File};
+
+use reqwest::get;
+
+const DNSURL: &str = "https://api.shodan.io/dns/domain/";
+
 
 fn main() {
-    let shodan_api_key = fs::read_to_string("/run/secrets/shodan_api_key").expect("Not able to read Shodan secret");
-    println!("{shodan_api_key}");
+    let shodan_api_key:String = fs::read_to_string("/run/secrets/shodan_api_key").expect("Not able to read Shodan secret");
+    let domains = File::open("/domains.txt").expect("Not able to read domains.txt file."); 
+
+    println!("{subdomains}");
 }
+
+async fn get_subdomains(domain:String) {
+    let ubdomains = get("https://www.rust-lang.org")
+    return subdomains;
+} 
+
