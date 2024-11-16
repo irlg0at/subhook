f4bfe2d416d9c6d5d46d7e7b988ab5d43c9611f7
irlg0at <34748847+irlg0at@users.noreply.github.com>
2024-11-16 21:10:41 +0100

    WIP: Shodan subdomain request

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
 use std::fs::{self, File};
-
+use std::io::{BufReader, BufRead};
 use reqwest::get;
+use tokio;
 
-const DNSURL: &str = "https://api.shodan.io/dns/domain/";
+const SHODANURL: &str = "https://api.shodan.io/dns/domain/";
 
-
-fn main() {
+#[tokio::main]
+async fn main() {
     let shodan_api_key:String = fs::read_to_string("/run/secrets/shodan_api_key").expect("Not able to read Shodan secret");
-    let domains = File::open("/domains.txt").expect("Not able to read domains.txt file."); 
+    let domains = BufReader::new(File::open("/domains.txt").expect("Not able to read domains.txt file.")); 
 
-    println!("{subdomains}");
+    for domain in domains.lines() {
+        let subdomains = get_shodan_subdomains(domain.unwrap(), &shodan_api_key).await.unwrap();
+        println!("{subdomains}");
+    }
 }
 
-async fn get_subdomains(domain:String) {
-    let ubdomains = get("https://www.rust-lang.org")
-    return subdomains;
+async fn get_shodan_subdomains(domain:String, shodan_api_key:&String) -> Option<String> {
+    match get(format!("{SHODANURL}{domain}?key={shodan_api_key}")).await {
+        Ok(response) => match response.text().await {
+            Ok(subdomains) => Some(subdomains),
+            Err(e) => {
+                eprintln!("Failed to read shodan response: {e}");
+                return None
+            }
+        },
+        Err(e) =>  {
+            eprint!("Failed to query shodan for subdomains: {e}");
+            return None
+        }
+    }
 } 
 
